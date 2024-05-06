9ed6610df8f19f47832e365806304613231260cc
LeProfDeGitHub <alexis.paillard@outlook.fr>
2024-05-06 11:02:59 +0200

    add convergence plot

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from display import plot_contour
 from methods import gradient_descent_optimal_step
 
 J = get_zvankin_quad(2)
-X_gd, _ = gradient_descent_optimal_step(J, np.array([[0], [0]]), 1e-3, 1000)
+X_gd, i_max = gradient_descent_optimal_step(J, np.array([[0], [0]]), 1e-3, 1000)
 
 plot_contour(J, (-10, 10), (-10, 10))
 
@@ -30,3 +30,8 @@ for i, y_val in enumerate(y):
     plt.plot(x, y_val)
     plt.savefig(f'figure/Coupe de la fonction ({i+1}).svg')
     plt.show()
+
+plt.cla()
+plt.title('Convergence de la solution')
+plt.plot(np.arange(i_max+1), [np.linalg.norm(grad_val) for grad_val in grad])
+plt.show()
