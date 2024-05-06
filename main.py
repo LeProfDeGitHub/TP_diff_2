a0d4e1e1d059c487af235b9de080ca98901a5cbf
LeProfDeGitHub <alexis.paillard@outlook.fr>
2024-05-06 11:46:04 +0200

    new_update

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from matplotlib import pyplot as plt
 import numpy as np
-from function import get_zvankin_quad , tri_diagonal
-from display import plot_contour
-from methods import gradient_descent_optimal_step
+from function import get_zvankin_quad
+from display import display_convergence_2d
+from methods import gradient_descent_fix_step, gradient_descent_optimal_step
 
 
 
+def main():
+    display_convergence_2d('figure\\grad_desc_fix_step',
+                            get_zvankin_quad(2),
+                            np.array([[0], [0]]),
+                            gradient_descent_fix_step)
+
+    display_convergence_2d('figure\\grad_desc_optimal_step',
+                            get_zvankin_quad(2),
+                            np.array([[0], [0]]),
+                            gradient_descent_optimal_step)
 
 
 
-
-
-plt.cla()
-plt.title('Convergence de la solution')
-plt.plot(np.arange(i_max+1), [np.linalg.norm(grad_val) for grad_val in grad])
-plt.show()
-
-
+if __name__ == '__main__':
+    main()
